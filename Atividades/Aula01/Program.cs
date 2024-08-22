177a02e3e4ac2870975a178712edea2adf2b2123
Yuliangel Herrera Silveira <418069@unoesc.lan>
2024-08-21 21:51:32 -0300

    Aula Classe, Objeto Enum

--- Atividades/Aula01/Program.cs
+++ Atividades/Aula01/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Comentario
+
+/* 
+   Comentario multiplas linhas
+*/
+
+// Declarando variavel
+using Aula01;
+
+int number;
+number = 10;
+
+ //Atribuindo valor
+const int DAYS_IN_WEEK = 7;
+
+//Imprimindo string concatenada
+Console.WriteLine($"a SEMANA TEM {DAYS_IN_WEEK} dias.");
+
+/*
+*/
+TipoEnum tipoEnum = new TipoEnum(); 
+
+TipoEnum.Language enumEnglish = tipoEnum.GetLanguageEnum("ingles");
+
+Console.WriteLine($"O enum de englês é {enumEnglish}");
