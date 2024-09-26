0ee978ef05836123737cab5db3590d42cc9d0f7c
Yuliangel Herrera <yuliangel01herrera@gmail.com>
2024-09-25 23:48:41 -0300

    Exercicios Listas

---
+++ Exercicioss/ListasCirculares/Program.cs
@@ -0,0 +1,94 @@
+﻿public class Musica
+{
+    public string Titulo { get; set; }
+    public string Artista { get; set; }
+    public string Genero { get; set; }
+
+    public Musica(string titulo, string artista, string genero)
+    {
+        Titulo = titulo;
+        Artista = artista;
+        Genero = genero;
+    }
+}
+
+public class Node<T>
+{
+    public T Value { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+        Next = null;
+    }
+}
+
+public class CircularLinkedList<T> : IEnumerable<T>
+{
+    public Node<T> Head { get; set; }
+
+    public CircularLinkedList()
+    {
+        Head = null;
+    }
+
+    public void Add(T value)
+    {
+        Node<T> newNode = new Node<T>(value);
+        if (Head == null)
+        {
+            Head = newNode;
+            newNode.Next = Head;
+        }
+        else
+        {
+            Node<T> current = Head;
+            while (current.Next != Head)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+            newNode.Next = Head;
+        }
+    }
+
+    public void PrintList()
+    {
+        Node<T> current = Head;
+        if (current != null)
+        {
+            do
+            {
+                Musica musica = (Musica)current.Value;
+                Console.WriteLine($"Título: {musica.Titulo}, Artista: {musica.Artista}, Gênero: {musica.Genero}");
+                current = current.Next;
+            } while (current != Head);
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node<T> current = Head;
+        if (current != null)
+        {
+            do
+            {
+                yield return current.Value;
+                current = current.Next;
+            } while (current != Head);
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+// Exemplo de uso
+CircularLinkedList<Musica> playlist = new CircularLinkedList<Musica>();
+playlist.Add(new Musica("Happy", "Pharrell Williams", "Pop"));
+playlist.Add(new Musica("Uptown Funk", "Mark Ronson ft. Bruno Mars", "Funk"));
+playlist.Add(new Musica("Can't Stop the Feeling!", "Justin Timberlake", "Pop"));
+playlist.PrintList();
---
+++ Exercicioss/ListasLinkadas/Program.cs
@@ -0,0 +1,72 @@
+﻿public class Node<T>
+{
+    public T Value { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T value)
+    {
+        Value = value;
+        Next = null;
+    }
+}
+
+public class LinkedList<T> : IEnumerable<T>
+{
+    public Node<T> Head { get; set; }
+
+    public LinkedList()
+    {
+        Head = null;
+    }
+
+    public void Add(T value)
+    {
+        Node<T> newNode = new Node<T>(value);
+        if (Head == null)
+        {
+            Head = newNode;
+        }
+        else
+        {
+            Node<T> current = Head;
+            while (current.Next != null)
+            {
+                current = current.Next;
+            }
+            current.Next = newNode;
+        }
+    }
+
+    public void PrintList()
+    {
+        Node<T> current = Head;
+        while (current != null)
+        {
+            Console.Write(current.Value + " ");
+            current = current.Next;
+        }
+        Console.WriteLine();
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node<T> current = Head;
+        while (current != null)
+        {
+            yield return current.Value;
+            current = current.Next;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+// Exemplo
+LinkedList<int> list = new LinkedList<int>();
+list.Add(02);
+list.Add(76);
+list.Add(89);
+list.PrintList(); 
