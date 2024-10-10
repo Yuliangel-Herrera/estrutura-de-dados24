04eaaa9d010a5aa3f6bac4a4013f475b523de354
Yuliangel Herrera <yuliangel01herrera@gmail.com>
2024-10-10 08:14:16 -0300

    InsertionSort

---
+++ Exercicioss/InsertionSort/InsertionSort/Program.cs
@@ -0,0 +1,17 @@
+﻿using InsertionSort;
+
+int[] nums = [0, 7, 6, 2, 20, 1];
+
+Console.Write('[');
+foreach(int i in nums){
+    Console.Write($"{i} ");
+}
+Console.Write(']');
+
+Console.Write(" Before sorting: ");
+InsertionSort.InsertionSort.Print(nums);
+InsertionSort.InsertionSort.Sort(nums);
+
+Console.Write(" After sorting: ");
+InsertionSort.InsertionSort.Print(nums);
+
